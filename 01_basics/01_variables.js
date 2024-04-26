53a721682250540e8f269ba534c81ba7d3c10a1d
Kisna Goyal <131574334+kisnagoyal@users.noreply.github.com>
2024-04-26 14:53:52 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const a = 10;
+var b = 20;      //less used
+let c = "hello"
+d = "no"              //less preferred
+
+console.log([a,b,c,d]);
+console.table([a,b,c,d]);
---
+++ 01_basics/02.datatypes.js
@@ -0,0 +1,4 @@
+"use strict"; // all codes will be treated as newer version of js
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
